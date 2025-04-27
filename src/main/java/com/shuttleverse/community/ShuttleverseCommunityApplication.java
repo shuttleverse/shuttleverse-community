b79161f85f10c584b84f9a856a8ec2d81d0d4d33
QW66666 <wusteven0829@gmail.com>
2025-04-27 17:05:43 -0400

    initial setup

---
+++ src/main/java/com/shuttleverse/community/ShuttleverseCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.shuttleverse.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttleverseCommunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShuttleverseCommunityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/shuttleverse/community/ShuttleverseCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuttleverse.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttleverseCommunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
