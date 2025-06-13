1ac3376f9be1dd0dce2f662af0199e1103b56aa5
qihongw08 <99198350+qihongw08@users.noreply.github.com>
2025-06-13 19:39:18 -0400

    Revert "Entity Creation Endpoints"
    This reverts commit fb0aaa99f58839891ddd87adb9586597d270a1a6.

--- src/main/java/com/shuttleverse/community/ShuttleverseCommunityApplication.java
+++ src/main/java/com/shuttleverse/community/ShuttleverseCommunityApplication.java
@@ -2,10 +2,8 @@ package com.shuttleverse.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ShuttleverseCommunityApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/shuttleverse/community/constants/DayOfWeek.java
+++
@@ -1,8 +0,0 @@
-package com.shuttleverse.community.constants;
-
-public enum DayOfWeek {
-  MONDAY(1), TUESDAY(2), WEDNESDAY(3), THURSDAY(4), FRIDAY(5), SATURDAY(6), SUNDAY(7);
-
-  DayOfWeek(Integer dayOfWeek) {
-  }
-}
